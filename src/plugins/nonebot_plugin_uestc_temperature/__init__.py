ba3ef0ad8586bc20a54c18842e620bf1c475ee09
Azide <rukuy@qq.com>
2022-03-24 22:55:55 +0800

    修改体温上报命令，添加数据查询功能，添加更新id功能（未完成）

--- src/plugins/nonebot_plugin_uestc_temperature/__init__.py
+++ src/plugins/nonebot_plugin_uestc_temperature/__init__.py
@@ -1,20 +1,55 @@
 from nonebot.plugin import on_command
-from nonebot.params import ArgStr
+from nonebot.params import ArgStr, CommandArg
 from nonebot.rule import to_me
 from nonebot.adapters.onebot.v11 import Bot, Message, MessageEvent
 from nonebot import logger
 
 from .upload import Reporter
+from .userData import addData, loadData
+
+import time
 
 temperature = on_command("体温上报", rule=to_me())
 
 
-@temperature.got("session_id", "成电智慧学工Session Id：")
+@temperature.handle()
+async def upload_temperature(user: str = CommandArg()):
+    logger.info(f"Get user:{user}")
+    user_data = loadData(user)
+    if user_data["Sid"] == "err-404":
+        msg = f"不存在用户：{user}\n请使用 添加填报人 命令添加"
+    else:
+        logger.info(f"Get user session id:{user_data['Sid']}")
+        reporter = Reporter(user_data["Sid"])
+        await temperature.send(Message(f"即将填报用户:{user}"))
+        result, state = reporter.run()
+        if state == "无效Session id":
+            timeStr = time.strftime(
+                "%Y-%m-%d %H:%M:%S", time.localtime(user_data["updateTime"])
+            )
+            msg = f"用户：{user}\n无效的Session id:\n{user_data['Sid']}\n上次更新时间为{timeStr}\n请发送 更新id 命令进行更新"
+        else:
+            msg = f"填报状态：{state}\n" + ("请自行手动填报" if not result else "")
+    logger.info(msg)
+    await temperature.finish(Message(msg))
+    """@temperature.got("session_id", "成电智慧学工Session Id：")
 async def upload_temperature(session_id: str = ArgStr()):
     logger.info(f"get id:{session_id}")
     reporter = Reporter(session_id)
     await temperature.send(Message(f"已接收:{session_id}"))
     result, state = reporter.run()
-    msg = f"填报状态：{state}\n" + ("请自行手动填报" if not result else "")
+    if state == '无效Session id':
+        lastUpdateTime=loadData()
+        msg= f"无效的Session id:\n{session_id}\n请发送 更新id 命令进行更新"
+    else:
+        msg = f"填报状态：{state}\n" + ("请自行手动填报" if not result else "")
     logger.info(f"{msg}")
-    await temperature.finish(Message(msg))
+    await temperature.finish(Message(msg))"""
+
+
+updateID = on_command("更新id", rule=to_me())
+
+
+@updateID.got("session_id", "成电智慧学工Session Id：")
+async def update_id(session_id: str = ArgStr()):
+    logger.info(f"get id:{session_id}")
--- src/plugins/nonebot_plugin_uestc_temperature/userData.py
+++ src/plugins/nonebot_plugin_uestc_temperature/userData.py
@@ -5,20 +5,20 @@ import json, os, time
 user_json_path = os.path.join(os.path.dirname(__file__), "user.json")
 
 
-def addData(user: str, Sid: str):
+def addData(user: str, Sid: str) -> bool:
     """追加新的user数据并打上时间戳"""
     nowtime = int(time.time())
-    newUser = {user: {"Sid": Sid, "date": nowtime}}
+    newUser = {user: {"Sid": Sid, "updateTime": nowtime}}
     try:
         if os.path.getsize(user_json_path) == 0:
             with open(user_json_path, "w+", encoding="utf-8") as ud:
-                json.dump(newUser, ud)
+                json.dump(newUser, ud, indent=4, ensure_ascii=False)
 
         else:
             allUser = {}
 
             with open(user_json_path, "r", encoding="utf-8") as ud_load:
-                allUser = json.load(ud_load)
+                allUser:dict = json.load(ud_load)
 
             allUser.update(newUser)
 
@@ -26,10 +26,26 @@ def addData(user: str, Sid: str):
                 json.dump(allUser, ud_write, indent=4, ensure_ascii=False)
 
     except Exception as e:
-        print(repr(e))
+        # logger.error(repr(e))
         return False
     else:
         return True
+
+
+def loadData(user: str) -> dict:
+    '''
+    #### 按照所记录的user查找数据 返回字典{Sid,updateTime}
+    #### 查找失败则返回'Sid':'err-404','date':0
+    '''
+    with open(user_json_path, "r", encoding="utf-8") as ud_load:
+        allUser = json.load(ud_load)
+    try:
+        getUser = allUser[user]
+    except Exception as e:
+        # logger.error(repr(e))
+        return {'Sid':'err-404','date':0}
+    else:
+        return getUser
 
 
 if __name__ == "__main__":
@@ -38,3 +54,11 @@ if __name__ == "__main__":
     sid = input("input sid:")
     result = addData(user, sid)
     print(f"result:{result}")
+    test = loadData(input("who:"))
+    print(test)
+    print(
+        test["Sid"]
+        + ("\n"
+        + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(test["updateTime"]))
+        if test["Sid"]!='err-404' else '')
+    )
